57eeca632f5279989f287d2292ec64a7caf9bfd4
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-11-30 15:29:05 -0500

    10. DBStorage - Amenity correction

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 import models
 from os import getenv
 
