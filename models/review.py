d0a569769597a72764c59077d9f78445685be8ea
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-11-30 12:18:32 -0500

    correction review

--- models/review.py
+++ models/review.py
@@ -2,11 +2,11 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
+
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
