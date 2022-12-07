3b40699b0a7092fdb824a89c62cb77ed7381f560
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-07 07:24:02 -0500

    7.13

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,12 +7,18 @@ from os import getenv
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from models.base_model import Base
-
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class DBStorage:
     """ manage storage of hbnb models using SQL Alchemy """
     __engine = None
     __session = None
+    all_classes = ["State", "City", "User", "Place", "Review"]
 
     def __init__(self):
         """Create the engine (self.__engine)"""
@@ -35,23 +41,19 @@ class DBStorage:
             of the class name
         """
 
-        dict_cls = {}
-        if cls is not None:
-            for obj in self.__session.query(cls).all():
-                dict_cls[obj.to_dict()['__class__'] + '.' + obj.id] = obj
+        dict_new = {}
+        if cls is None:
+            for c in self.all_classes:
+                c = eval(c)
+                for instance in self.__session.query(c).all():
+                    key = instance.__class__.__name__ + '.' + instance.id
+                    dict_new[key] = instance
         else:
-            from models.user import User
-            from models.place import Place
-            from models.state import State
-            from models.city import City
-            from models.amenity import Amenity
-            from models.review import Review
+            for instance in self.__session.query(cls).all():
+                key = instance.__class__.__name__+'.' + instance.id
+                dict_new[key] = instance
 
-            list_class = [State, Place, User, City, Review, Amenity]
-            for every_class in list_class:
-                for ob in self.__session.query(every_class).all():
-                    dict_cls[ob.to_dict()['__class__'] + '.' + ob.id] = ob
-        return dict_cls
+        return dict_new
 
     def new(self, obj):
         """add the object to the current database session"""
@@ -63,19 +65,12 @@ class DBStorage:
 
     def delete(self, obj=None):
         """Delete from the current database session obj if not None"""
-        if obj is not None:
+        if obj is not:
             self.__session.delete(obj)
 
     def reload(self):
         """Create all tables in the database
         and create the current database session by by using a sessionmaker """
-        from models.base_model import BaseModel, Base
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         Base.metadata.create_all(self.__engine)
 
